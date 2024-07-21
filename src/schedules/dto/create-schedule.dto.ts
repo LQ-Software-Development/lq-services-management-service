b5a67574b36d85bf0556d33c6cdcab110bf1cee4
QuadriniL <quadrinho.tiolu@gmail.com>
2024-07-21 18:38:36 -0300

    feat: Add eachDayRepeat and finalRepeatDate fields to CreateScheduleDto

--- src/schedules/dto/create-schedule.dto.ts
+++ src/schedules/dto/create-schedule.dto.ts
@@ -1,41 +1,50 @@
-import { IsDateString, IsOptional, IsString } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger";
+import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateScheduleDto {
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  clientId?: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    clientId?: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  clientName?: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    clientName?: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  description?: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    description?: string;
+  @ApiProperty({
+    example: new Date(),
+  })
+  @IsDateString()
+  date: string;
 
-    @ApiProperty({
-        example: new Date()
-    })
-    @IsDateString()
-    date: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  groupId?: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    groupId?: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  organizationId?: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    organizationId?: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  serviceId?: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    serviceId?: string;
+  @ApiPropertyOptional()
+  @IsNumber()
+  @IsOptional()
+  eachDayRepeat?: number;
+
+  @ApiPropertyOptional()
+  @IsDateString()
+  @IsOptional()
+  finalRepeatDate?: string;
 }
--- src/schedules/service/create-schedules.service.ts
+++ src/schedules/service/create-schedules.service.ts
@@ -12,6 +12,27 @@ export class CreateSchedulesService {
   ) {}
 
   execute(createScheduleDto: CreateScheduleDto) {
-    return this.scheduleRepository.save(createScheduleDto) ;
+    const schedules = [];
+
+    if (createScheduleDto.eachDayRepeat && createScheduleDto.finalRepeatDate) {
+      const finalRepeatDate = new Date(createScheduleDto.finalRepeatDate);
+      const date = new Date(createScheduleDto.date);
+
+      while (date <= finalRepeatDate) {
+        schedules.push({
+          ...createScheduleDto,
+          date: date.toISOString(),
+        });
+
+        date.setDate(date.getDate() + createScheduleDto.eachDayRepeat);
+      }
+    } else {
+      delete createScheduleDto.eachDayRepeat;
+      delete createScheduleDto.finalRepeatDate;
+
+      schedules.push(createScheduleDto);
+    }
+
+    return this.scheduleRepository.save(createScheduleDto);
   }
 }
