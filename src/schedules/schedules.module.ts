e57e6bdf8b984ffa2c7fb16d67713d7bc55dc21a
humberto <humberto.gnm98@gmail.com>
2025-10-30 13:39:34 -0300

    Fix: import jwt on schedule

--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -1,17 +1,24 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { SchedulesController } from './schedules.controller';
-import { CreateSchedulesService } from './services/create-schedules.service';
-import { ListSchedulesService } from './services/list-schedules.service';
-import { DeleteSchedulesService } from './services/delete-schedules.service';
-import { FetchScheduleService } from './services/fetch-schedule.service';
-import { UpdateScheduleService } from './services/update-schedule.service';
-import { DeleteClientSchedulesService } from './services/delete-client-schedules.service';
-import { FinancialApiProvider } from 'src/providers/financial-api.provider';
-import { Schedule } from 'src/models/schedule.entity';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
+import { SchedulesController } from "./schedules.controller";
+import { CreateSchedulesService } from "./services/create-schedules.service";
+import { ListSchedulesService } from "./services/list-schedules.service";
+import { DeleteSchedulesService } from "./services/delete-schedules.service";
+import { FetchScheduleService } from "./services/fetch-schedule.service";
+import { UpdateScheduleService } from "./services/update-schedule.service";
+import { DeleteClientSchedulesService } from "./services/delete-client-schedules.service";
+import { FinancialApiProvider } from "src/providers/financial-api.provider";
+import { Schedule } from "src/models/schedule.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Schedule])],
+  imports: [
+    TypeOrmModule.forFeature([Schedule]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || "defaultSecret",
+      signOptions: { expiresIn: "1h" },
+    }),
+  ],
   controllers: [SchedulesController],
   providers: [
     CreateSchedulesService,
